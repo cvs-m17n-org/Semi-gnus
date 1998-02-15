43862bd9cbf4efd8cfa5ade6e3ad6c4bdeb1176d
MORIOKA Tomohiko <tomo@kanji.zinbun.kyoto-u.ac.jp>
1998-02-15 15:54:13 +0000

    Sync up with qgnus-0.25.

--- lisp/gnus-sum.el
+++ lisp/gnus-sum.el
@@ -1399,6 +1399,7 @@ increase the score of each group you read."
     "l" gnus-article-date-local
     "e" gnus-article-date-lapsed
     "o" gnus-article-date-original
+    "i" gnus-article-date-iso8601
     "s" gnus-article-date-user)
 
   (gnus-define-keys (gnus-summary-wash-empty-map "E" gnus-summary-wash-map)
@@ -1600,6 +1601,7 @@ increase the score of each group you read."
 	["Citation" gnus-article-highlight-citation t])
        ("Date"
 	["Local" gnus-article-date-local t]
+	["ISO8601" gnus-article-date-iso8601 t]
 	["UT" gnus-article-date-ut t]
 	["Original" gnus-article-date-original t]
 	["Lapsed" gnus-article-date-lapsed t]
@@ -2187,10 +2189,12 @@ article number."
 	 (gnus-summary-last-subject))))
 
 (defmacro gnus-summary-article-header (&optional number)
+  "Return the header of article NUMBER."
   `(gnus-data-header (gnus-data-find
 		      ,(or number '(gnus-summary-article-number)))))
 
 (defmacro gnus-summary-thread-level (&optional number)
+  "Return the level of thread that starts with article NUMBER."
   `(if (and (eq gnus-summary-make-false-root 'dummy)
 	    (get-text-property (point) 'gnus-intangible))
        0
@@ -2198,10 +2202,12 @@ article number."
 		       ,(or number '(gnus-summary-article-number))))))
 
 (defmacro gnus-summary-article-mark (&optional number)
+  "Return the mark of article NUMBER."
   `(gnus-data-mark (gnus-data-find
 		    ,(or number '(gnus-summary-article-number)))))
 
 (defmacro gnus-summary-article-pos (&optional number)
+  "Return the position of the line of article NUMBER."
   `(gnus-data-pos (gnus-data-find
 		   ,(or number '(gnus-summary-article-number)))))
 
@@ -2224,6 +2230,7 @@ article number."
        gnus-summary-default-score 0))
 
 (defun gnus-summary-article-children (&optional number)
+  "Return a list of article numbers that are children of article NUMBER."
   (let* ((data (gnus-data-find-list (or number (gnus-summary-article-number))))
 	 (level (gnus-data-level (car data)))
 	 l children)
@@ -2235,6 +2242,7 @@ article number."
     (nreverse children)))
 
 (defun gnus-summary-article-parent (&optional number)
+  "Return the article number of the parent of article NUMBER."
   (let* ((data (gnus-data-find-list (or number (gnus-summary-article-number))
 				    (gnus-data-list t)))
 	 (level (gnus-data-level (car data))))
@@ -2259,6 +2267,11 @@ This is all marks except unread, ticked, dormant, and expirable."
 	   (= mark gnus-expirable-mark))))
 
 (defmacro gnus-article-mark (number)
+  "Return the MARK of article NUMBER.
+This macro should only be used when computing the mark the \"first\"
+time; i.e., when generating the summary lines.  After that,
+`gnus-summary-article-mark' should be used to examine the
+marks of articles."
   `(cond
     ((memq ,number gnus-newsgroup-unsendable) gnus-unsendable-mark)
     ((memq ,number gnus-newsgroup-undownloaded) gnus-undownloaded-mark)
@@ -5082,6 +5095,8 @@ gnus-exit-group-hook is called with no arguments if that value is non-nil."
       (gnus-dup-enter-articles))
     (when gnus-use-trees
       (gnus-tree-close group))
+    ;; Remove entries for this group.
+    (nnmail-purge-split-history group)
     ;; Make all changes in this group permanent.
     (unless quit-config
       (run-hooks 'gnus-exit-group-hook)
@@ -7071,6 +7086,9 @@ and `request-accept' functions."
 	      (gnus-request-replace-article
 	       article gnus-newsgroup-name (current-buffer)))))
 
+	;;;!!!Why is this necessary?
+	(set-buffer gnus-summary-buffer)
+	
 	(gnus-summary-goto-subject article)
 	(when (eq action 'move)
 	  (gnus-summary-mark-article article gnus-canceled-mark))))
