2047734e1db29917739863db51210367e6a2f9fb
MORIOKA Tomohiko <tomo@kanji.zinbun.kyoto-u.ac.jp>
1998-02-28 18:36:05 +0000

    Modify header.

--- lisp/gnus-ems.el
+++ lisp/gnus-ems.el
@@ -1,4 +1,4 @@
-;;; gnus-ems.el --- functions for making Gnus work under different Emacsen
+;;; gnus-ems.el --- functions for making Semi-gnus work under different Emacsen
 ;; Copyright (C) 1995,96,97,98 Free Software Foundation, Inc.
 
 ;; Author: Lars Magne Ingebrigtsen <larsi@ifi.uio.no>
