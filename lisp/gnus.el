f55d26124661f36fa536d37b8fefc0c8153b5f49
MORIOKA Tomohiko <tomo@kanji.zinbun.kyoto-u.ac.jp>
1998-06-27 16:55:22 +0000

    (gnus-version-number): Update to 6.7.6.

--- lisp/gnus.el
+++ lisp/gnus.el
@@ -250,7 +250,7 @@ is restarted, and sometimes reloaded."
   :link '(custom-manual "(gnus)Exiting Gnus")
   :group 'gnus)
 
-(defconst gnus-version-number "6.7.5"
+(defconst gnus-version-number "6.7.6"
   "Version number for this version of gnus.")
 
 (defconst gnus-version
