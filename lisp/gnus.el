018281887360046e6d84bb79657c31b3395eeeaa
Shuhei KOBAYASHI <shuhei-k@jaist.ac.jp>
1998-07-23 16:39:24 +0000

    (gnus-version-number): Update to 6.8.2. (gnus-version): Change to "Semi-gnus". Change comment format.

--- lisp/gnus.el
+++ lisp/gnus.el
@@ -250,11 +250,11 @@ is restarted, and sometimes reloaded."
   :link '(custom-manual "(gnus)Exiting Gnus")
   :group 'gnus)
 
-(defconst gnus-version-number "6.8.1"
+(defconst gnus-version-number "6.8.2"
   "Version number for this version of gnus.")
 
 (defconst gnus-version
-  (format "Chao-gnus %s (based on Gnus 5.6.24; for SEMI 1.8/FLIM 1.8)"
+  (format "Semi-gnus %s (based on Gnus 5.6.24; for SEMI 1.8, FLIM 1.8/1.9)"
           gnus-version-number)
   "Version string for this version of gnus.")
 
