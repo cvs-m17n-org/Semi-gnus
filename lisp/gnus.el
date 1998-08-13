3b79a0c1944f6a0976b366c7ca37e1a9d077afeb
Tatsuya Ichikawa <t-ichi@po.shiojiri.ne.jp>
1998-08-13 02:40:32 +0000

    Sync up with gnus-5.6.34

--- lisp/gnus.el
+++ lisp/gnus.el
@@ -250,7 +250,7 @@ is restarted, and sometimes reloaded."
   :link '(custom-manual "(gnus)Exiting Gnus")
   :group 'gnus)
 
-(defconst gnus-version-number "6.8.10"
+(defconst gnus-version-number "6.8.11"
   "Version number for this version of gnus.")
 
 (defconst gnus-version
