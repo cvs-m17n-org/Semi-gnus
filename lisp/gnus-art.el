e3f7b37fde93bca1c24c7dba5ea7522717aebd41
Katsumi Yamaoka <yamaoka@jpl.org>
1998-09-08 22:00:44 +0000

    Importing pgnus-0.22.

--- lisp/gnus-art.el
+++ lisp/gnus-art.el
@@ -545,6 +545,10 @@ displayed by the first non-nil matching CONTENT face."
 			       (item :tag "skip" nil)
 			       (face :value default)))))
 
+(defcustom gnus-article-decode-hook
+  '(gnus-article-decode-charset gnus-article-decode-rfc1522)
+  "*Hook run to decode charsets in articles.")
+
 ;;; Internal variables
 
 (defvar article-lapsed-timer nil)
@@ -806,7 +810,7 @@ always hide."
 (defun article-treat-dumbquotes ()
   "Translate M******** sm*rtq**t*s into proper text."
   (interactive)
-  (article-translate-characters "\221\222\223\223" "`'\"\""))
+  (article-translate-characters "\221\222\223\224" "`'\"\""))
 
 (defun article-translate-characters (from to)
   "Translate all characters in the body of the article according to FROM and TO.
@@ -2538,6 +2542,9 @@ If given a prefix, show the hidden text instead."
 	    (insert-buffer-substring gnus-article-buffer))
 	  (setq gnus-original-article (cons group article))))
 
+      ;; Decode charsets.
+      (run-hooks 'gnus-article-decode-hook)
+      
       ;; Update sparse articles.
       (when (and do-update-line
 		 (or (numberp article)
