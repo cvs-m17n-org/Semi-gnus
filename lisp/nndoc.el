0bb1cbd87aa246bd4a2bba65c651e728d15c9937
Tatsuya Ichikawa <t-ichi@po.shiojiri.ne.jp>
1998-09-24 14:21:22 +0000

    Importing pgnus-0.32

--- lisp/nndoc.el
+++ lisp/nndoc.el
@@ -290,6 +290,7 @@ from the document.")
       (setq nndoc-dissection-alist nil)
       (save-excursion
 	(set-buffer nndoc-current-buffer)
+	(mm-enable-multibyte)
 	(erase-buffer)
 	(if (stringp nndoc-address)
 	    (nnheader-insert-file-contents nndoc-address)
