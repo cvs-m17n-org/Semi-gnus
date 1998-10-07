abc0390ade870f3cf504f59962b33937ebd44187
Keiichi Suzuki <keiichi@nanap.org>
1998-10-07 01:15:59 +0000

    Change version to Nana-gnus 6.8.19.1.

--- lisp/gnus.el
+++ lisp/gnus.el
@@ -250,11 +250,11 @@ is restarted, and sometimes reloaded."
   :link '(custom-manual "(gnus)Exiting Gnus")
   :group 'gnus)
 
-(defconst gnus-version-number "6.8.19"
+(defconst gnus-version-number "6.8.19.1"
   "Version number for this version of gnus.")
 
 (defconst gnus-version
-  (format "Semi-gnus %s (based on Gnus 5.6.44; for SEMI 1.8, FLIM 1.8/1.9)"
+  (format "Nana-gnus %s (based on Gnus 5.6.44; for SEMI 1.8, FLIM 1.8/1.9)"
           gnus-version-number)
   "Version string for this version of gnus.")
 
