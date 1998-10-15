3fce4417dfb00cf9a96f40979f5445baa8250827
Keiichi Suzuki <keiichi@nanap.org>
1998-10-15 05:46:42 +0000

    gnus-update-format-specifications: Force update `gnus-format-specs' when `gnus-version' is differ from saved version.

--- lisp/gnus-spec.el
+++ lisp/gnus-spec.el
@@ -169,7 +169,10 @@
   ;; See whether all the stored info needs to be flushed.
   (when (or force
 	    (not (equal emacs-version
-			(cdr (assq 'version gnus-format-specs)))))
+			(cdr (assq 'version gnus-format-specs))))
+	    (not (equal gnus-version
+			(cdr (assq 'gnus-version gnus-format-specs)))))
+    (message "%s" "Force update format specs.")
     (setq gnus-format-specs nil))
 
   ;; Go through all the formats and see whether they need updating.
@@ -214,7 +217,9 @@
 	  (set (intern (format "gnus-%s-line-format-spec" type)) val)))))
 
   (unless (assq 'version gnus-format-specs)
-    (push (cons 'version emacs-version) gnus-format-specs)))
+    (push (cons 'version emacs-version) gnus-format-specs))
+  (unless (assq 'gnus-version gnus-format-specs)
+    (push (cons 'gnus-version gnus-version) gnus-format-specs)))
 
 (defvar gnus-mouse-face-0 'highlight)
 (defvar gnus-mouse-face-1 'highlight)
