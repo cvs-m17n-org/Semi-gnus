7d21143d10afcf264389f5247b427b8ddc508136
MORIOKA Tomohiko <tomo@kanji.zinbun.kyoto-u.ac.jp>
1998-10-29 10:59:33 +0000

    (gnus-version-number): Update to 6.12.1.

--- lisp/gnus.el
+++ lisp/gnus.el
@@ -253,7 +253,7 @@ is restarted, and sometimes reloaded."
 (defconst gnus-product-name "Chao-gnus"
   "Product name of this version of gnus.")
 
-(defconst gnus-version-number "6.12.0"
+(defconst gnus-version-number "6.12.1"
   "Version number for this version of gnus.")
 
 (defconst gnus-version
