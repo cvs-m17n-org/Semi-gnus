e23978f34e20ce837bc1dec7f75d11341445124d
Tatsuya Ichikawa <t-ichi@po.shiojiri.ne.jp>
1998-12-01 02:28:45 +0000

    Sync up with pgnus-0.57

--- lisp/gnus.el
+++ lisp/gnus.el
@@ -259,10 +259,10 @@ is restarted, and sometimes reloaded."
 (defconst gnus-product-name "T-gnus"
   "Product name of this version of gnus.")
 
-(defconst gnus-version-number "6.10.042"
+(defconst gnus-version-number "6.10.043"
   "Version number for this version of gnus.")
 
-(defconst gnus-original-version-number "0.56"
+(defconst gnus-original-version-number "0.57"
     "Version number for this version of Gnus.")
 
 (defconst gnus-original-product-name "Pterodactyl Gnus"
