6c8ba3324bc379d7a95c819de41b950611dfb7ee
Katsumi Yamaoka <yamaoka@jpl.org>
1998-12-14 07:36:57 +0000

    (gnus-version-number): Update to 6.9.2.

--- lisp/gnus.el
+++ lisp/gnus.el
@@ -253,7 +253,7 @@ is restarted, and sometimes reloaded."
 (defconst gnus-product-name "Semi-gnus"
   "Product name of this version of gnus.")
 
-(defconst gnus-version-number "6.9.1"
+(defconst gnus-version-number "6.9.2"
   "Version number for this version of gnus.")
 
 (defconst gnus-version
