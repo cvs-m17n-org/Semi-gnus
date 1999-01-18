e9448a835400d74e25d36927db3730950ff1eb19
Katsumi Yamaoka <yamaoka@jpl.org>
1999-01-18 00:14:51 +0000

    * (nnmail-pathname-coding-system): Default to binary.

--- lisp/nnmail.el
+++ lisp/nnmail.el
@@ -504,8 +504,7 @@ parameter.  It should return nil, `warn' or `delete'."
 	  t)
       (file-error nil))))
 
-(defvar nnmail-pathname-coding-system
-  'iso-8859-1
+(defvar nnmail-pathname-coding-system 'binary
   "*Coding system for pathname.")
 
 (defun nnmail-group-pathname (group dir &optional file)
