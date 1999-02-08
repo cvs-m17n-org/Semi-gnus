88b90631edec79900ea403af15f5a515d05d5670
Katsumi Yamaoka <yamaoka@jpl.org>
1999-02-08 12:07:53 +0000

    * gnus.el (gnus-version-number): Update to 6.10.5. * gnus-sum.el (gnus-articles-to-read): If the number of unread articles is more than `gnus-large-newsgroup', use it in default instead of the maximum number. (gnus-mime-extract-multipart): New function; add it to the acting-condition for extracting mulitpart messages. (gnus-mime-extract-message/rfc822): New function; add it to the acting-condition for extracting message/rfc822 or message/news.

--- lisp/gnus.el
+++ lisp/gnus.el
@@ -253,7 +253,7 @@ is restarted, and sometimes reloaded."
 (defconst gnus-product-name "Semi-gnus"
   "Product name of this version of gnus.")
 
-(defconst gnus-version-number "6.10.4"
+(defconst gnus-version-number "6.10.5"
   "Version number for this version of gnus.")
 
 (defconst gnus-version
