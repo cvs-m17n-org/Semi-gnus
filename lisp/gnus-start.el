64449126abfa33635b52eb780cf4401e0edaf8a4
Tetsuo Tsukamoto <czkmt@remus.dti.ne.jp>
1999-02-18 22:01:25 +0000

    * lisp/gnus.el (gnus-revision-number): Increment to 09. * lisp/gnus-start.el (save-buffers-kill-emacs): Rewrite the advice. Call `gnus-offer-save-summaries'.

--- lisp/gnus-start.el
+++ lisp/gnus-start.el
@@ -2304,8 +2304,10 @@ If FORCE is non-nil, the .newsrc file is read."
 ;; Call the function above at C-x C-c.
 (defadvice save-buffers-kill-emacs (before save-gnus-newsrc-file-maybe activate)
   "Save .newsrc and .newsrc.eld when Emacs is killed."
-  (if (get-buffer gnus-group-buffer)
-      (gnus-save-newsrc-file)))
+  (when (get-buffer gnus-group-buffer)
+    (gnus-run-hooks 'gnus-exit-gnus-hook)
+    (gnus-offer-save-summaries)
+    (gnus-save-newsrc-file)))
 
 (defun gnus-gnus-to-quick-newsrc-format ()
   "Insert Gnus variables such as gnus-newsrc-alist in lisp format."
