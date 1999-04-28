040b2066cb50ffea3168d2403aefc0b6c5df74be
Keiichi Suzuki <keiichi@nanap.org>
1999-04-28 05:11:07 +0000

    (gnus-version-number): Update to 6.12.15.

--- lisp/gnus.el
+++ lisp/gnus.el
@@ -253,7 +253,7 @@ is restarted, and sometimes reloaded."
 (defconst gnus-product-name "Nana-gnus"
   "Product name of this version of gnus.")
 
-(defconst gnus-version-number "6.12.14"
+(defconst gnus-version-number "6.12.15"
   "Version number for this version of gnus.")
 
 (defconst gnus-version
