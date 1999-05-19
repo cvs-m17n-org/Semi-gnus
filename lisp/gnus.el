1deb5a7d7e61c8a42df6b566d6cac1e4e97262f3
Katsumi Yamaoka <yamaoka@jpl.org>
1999-05-19 02:07:50 +0000

    (gnus-revision-number): Increment to 09.

--- lisp/gnus.el
+++ lisp/gnus.el
@@ -267,7 +267,7 @@ is restarted, and sometimes reloaded."
 (defconst gnus-version-number "6.10.064"
   "Version number for this version of gnus.")
 
-(defconst gnus-revision-number "08"
+(defconst gnus-revision-number "09"
   "Revision number for this version of gnus.")
 
 (defconst gnus-original-version-number "0.84"
