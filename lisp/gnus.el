91974e7a80bda21c036c02a4bf4ce64541da4b51
Katsumi Yamaoka <yamaoka@jpl.org>
1999-06-15 05:15:15 +0000

    (gnus-version-number): Update to 6.10.067. (gnus-revision-number): Fresh start from 00.

--- lisp/gnus.el
+++ lisp/gnus.el
@@ -264,13 +264,13 @@ is restarted, and sometimes reloaded."
 (defconst gnus-product-name "T-gnus"
   "Product name of this version of gnus.")
 
-(defconst gnus-version-number "6.10.066"
+(defconst gnus-version-number "6.10.067"
   "Version number for this version of gnus.")
 
 (defconst gnus-revision-number "01"
   "Revision number for this version of gnus.")
 
-(defconst gnus-original-version-number "0.87"
+(defconst gnus-original-version-number "0.88"
     "Version number for this version of Gnus.")
 
 (provide 'running-pterodactyl-gnus-0_73-or-later)
