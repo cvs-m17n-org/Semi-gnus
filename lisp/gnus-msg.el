a58748e3e7ff9aa624a177650950357817f4fe68
Katsumi Yamaoka <yamaoka@jpl.org>
1999-06-21 12:19:39 +0000

    (gnus-extended-version): Move `gnus-revision-number' to the last.

--- lisp/gnus-msg.el
+++ lisp/gnus-msg.el
@@ -622,11 +622,12 @@ If SILENT, don't prompt the user."
 (defun gnus-extended-version ()
   "Stringified gnus version."
   (concat gnus-product-name "/" gnus-version-number
+	  " (based on "
+	  gnus-original-product-name " v" gnus-original-version-number ")"
 	  (if (zerop (string-to-number gnus-revision-number))
 	      ""
 	    (concat " (revision " gnus-revision-number ")"))
-	  " (based on "
-	  gnus-original-product-name " v" gnus-original-version-number ")"))
+	  ))
 
 (defun gnus-message-make-user-agent (&optional include-mime-info max-column)
   "Return user-agent info.
