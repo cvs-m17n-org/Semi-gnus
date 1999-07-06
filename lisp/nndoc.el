19ac1bc69e7e0dc4c817290504edab79fed1b684
Katsumi Yamaoka <yamaoka@jpl.org>
1999-07-06 10:59:12 +0000

    Importing Pterodactyl Gnus v0.92.

--- lisp/nndoc.el
+++ lisp/nndoc.el
@@ -341,6 +341,9 @@ from the document.")
 		(setq entry (pop alist)))
       (when (memq subtype (or (cdr (assq 'subtype entry)) '(guess)))
 	(goto-char (point-min))
+	;; Remove blank lines.
+	(while (eq (following-char) ?\n)
+	  (delete-char 1))
 	(when (numberp (setq result (funcall (intern
 					      (format "nndoc-%s-type-p"
 						      (car entry))))))
@@ -581,6 +584,9 @@ from the document.")
     (save-excursion
       (set-buffer nndoc-current-buffer)
       (goto-char (point-min))
+      ;; Remove blank lines.
+      (while (eq (following-char) ?\n)
+	(delete-char 1))
       ;; Find the beginning of the file.
       (when nndoc-file-begin
 	(nndoc-search nndoc-file-begin))
