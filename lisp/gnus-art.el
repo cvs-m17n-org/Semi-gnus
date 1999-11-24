1cea8c73f89755ca7e089e4a93dd26cbdc6dc546
Katsumi Yamaoka <yamaoka@jpl.org>
1999-11-24 13:37:19 +0000

    (gnus-article-setup-highlight-words): Use `copy-sequence' instead of `copy-list'.

--- lisp/gnus-art.el
+++ lisp/gnus-art.el
@@ -2272,12 +2272,12 @@ This format is defined by the `gnus-article-time-format' variable."
 	       (while (setq elem (pop alist))
 		 (when (and name (string-match (car elem) name))
 		   (setq alist nil
-			 highlight (copy-list (cdr elem)))))
+			 highlight (copy-sequence (cdr elem)))))
 	       highlight)
-	     (copy-list highlight-words)
+	     (copy-sequence highlight-words)
 	     (if gnus-newsgroup-name
-		 (copy-list (gnus-group-find-parameter
-			     gnus-newsgroup-name 'highlight-words t)))
+		 (copy-sequence (gnus-group-find-parameter
+				 gnus-newsgroup-name 'highlight-words t)))
 	     gnus-emphasis-alist)))))
 
 (defvar gnus-summary-article-menu)
