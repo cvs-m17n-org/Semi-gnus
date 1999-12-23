63eb908b7692359cc4a0a72ea781d3e2e2bfeada
Keiichi Suzuki <keiichi@nanap.org>
1999-12-23 10:09:52 +0000

    (gnus-request-accept-article): Do not encode message.

--- lisp/gnus-int.el
+++ lisp/gnus-int.el
@@ -456,11 +456,6 @@ If GROUP is nil, all groups on GNUS-COMMAND-METHOD are scanned."
   (goto-char (point-max))
   (unless (bolp)
     (insert "\n"))
-  (unless no-encode
-    (save-restriction
-      (message-narrow-to-head)
-      (mail-encode-encoded-word-buffer))
-    (message-encode-message-body))
   (let ((func (car (or gnus-command-method
 		       (gnus-find-method-for-group group)))))
     (funcall (intern (format "%s-request-accept-article" func))
@@ -469,11 +464,6 @@ If GROUP is nil, all groups on GNUS-COMMAND-METHOD are scanned."
 	     last)))
 
 (defun gnus-request-replace-article (article group buffer &optional no-encode)
-  (unless no-encode
-    (save-restriction
-      (message-narrow-to-head)
-      (mail-encode-encoded-word-buffer))
-    (message-encode-message-body))
   (let ((func (car (gnus-group-name-to-method group))))
     (funcall (intern (format "%s-request-replace-article" func))
 	     article (gnus-group-real-name group) buffer)))
