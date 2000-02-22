dc9e85f99e557063ee0d4e8bb48b1535cf0fcf60
Keiichi Suzuki <keiichi@nanap.org>
2000-02-22 04:58:04 +0000

    (gnus-version-number): Update to 7.1.0.8.

--- lisp/gnus.el
+++ lisp/gnus.el
@@ -270,7 +270,7 @@ is restarted, and sometimes reloaded."
 (defconst gnus-product-name "Nana-gnus"
   "Product name of this version of gnus.")
 
-(defconst gnus-version-number "7.1.0.7"
+(defconst gnus-version-number "7.1.0.8"
   "Version number for this version of gnus.")
 
 (defconst gnus-version
