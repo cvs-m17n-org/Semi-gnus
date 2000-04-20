ecf6b6f8cfafbc7025f6aa1b1eaeab60d3958a3d
Katsumi Yamaoka <yamaoka@jpl.org>
2000-04-20 23:16:00 +0000

    Remove RCS cookie.

--- contrib/vcard.el
+++ contrib/vcard.el
@@ -7,8 +7,6 @@
 ;; Keywords: extensions
 ;; Created: 1997-09-27
 
-;; $Id: vcard.el,v 1.1.2.1 2000-04-18 22:55:01 yamaoka Exp $
-
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 2, or (at your option)
