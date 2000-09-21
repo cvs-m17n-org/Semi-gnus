09944b1f00796819526e57f4549459a58bbc3f97
Katsumi Yamaoka <yamaoka@jpl.org>
2000-09-21 00:11:02 +0000

    Synch with Gnus.

--- lisp/gnus-score.el
+++ lisp/gnus-score.el
@@ -2585,7 +2585,7 @@ GROUP using BNews sys file syntax."
 	      ;; too much.
 	      (delete-char (min (1- (point-max)) klen))
 	    (goto-char (point-max))
-	    (search-backward "/")
+	    (search-backward (char-to-string directory-sep-char))
 	    (delete-region (1+ (point)) (point-min)))
 	  ;; If short file names were used, we have to translate slashes.
 	  (goto-char (point-min))
