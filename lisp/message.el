11a639ac57892d456299c26558b4dd95e6c53686
Katsumi Yamaoka <yamaoka@jpl.org>
2001-02-15 01:09:47 +0000

    * lisp/message.el (message-cite-original-without-signature): Extract from  field for the simple citation line.

--- lisp/message.el
+++ lisp/message.el
@@ -2595,7 +2595,18 @@ be added to \"References\" field.
 	 (when message-indent-citation-function
 	   (if (listp message-indent-citation-function)
 	       message-indent-citation-function
-	     (list message-indent-citation-function)))))
+	     (list message-indent-citation-function))))
+	(message-reply-headers (or message-reply-headers
+				   (make-mail-header))))
+    (mail-header-set-from message-reply-headers
+			  (save-restriction
+			    (narrow-to-region
+			     (point)
+			     (if (search-forward "\n\n" nil t)
+				 (1- (point))
+			       (point-max)))
+			    (or (message-fetch-field "from")
+				"unknown sender")))
     ;; Allow undoing.
     (undo-boundary)
     (goto-char end)
