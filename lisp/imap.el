5bed12322319b17449686f1d0dfc66145870f9cd
Katsumi Yamaoka <yamaoka@jpl.org>
2001-04-16 02:01:34 +0000

    Synch with Oort Gnus.

--- lisp/imap.el
+++ lisp/imap.el
@@ -644,12 +644,12 @@ If ARGS, PROMPT is used as an argument to `format'."
 		      (not (imap-parse-greeting)))
 	    (accept-process-output process 1)
 	    (sit-for 1))
-	  (erase-buffer)
 	  (and imap-log
 	       (with-current-buffer (get-buffer-create imap-log)
 		 (buffer-disable-undo)
 		 (goto-char (point-max))
 		 (insert-buffer-substring buffer)))
+	  (erase-buffer)
 	  (when (memq (process-status process) '(open run))
 	    (setq done process)))))
     (if done
