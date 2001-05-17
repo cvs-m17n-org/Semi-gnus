6971b8bc85553dea831c9260ec6666b2068b3b7f
Katsumi Yamaoka <yamaoka@jpl.org>
2001-05-17 08:24:16 +0000

    * dgnushack.el (dgnushack-bind-colon-keywords): Don't ignore  `widget-convert-button', `widget-create' and `widget-put';  ignore `defface'.

--- lisp/dgnushack.el
+++ lisp/dgnushack.el
@@ -295,10 +295,9 @@ to the specified name LIBRARY (a la calling `load' instead of `load-library')."
 		   (while form
 		     (setq elem (pop form))
 		     (unless (memq (car-safe elem)
-				   '(\` backquote defcustom defgroup
-				     define-widget quote
-				     widget-convert-button
-				     widget-create widget-put))
+				   '(\` backquote
+				     defcustom defface defgroup
+				     define-widget quote))
 		       (while (consp elem)
 			 (push (car elem) form)
 			 (setq elem (cdr elem)))
