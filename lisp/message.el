655405bb183498a57f315f6474fbe1b165cf8c9b
Katsumi Yamaoka <yamaoka@jpl.org>
2001-07-24 22:28:47 +0000

    Synch with Oort Gnus.

--- lisp/message.el
+++ lisp/message.el
@@ -5383,18 +5383,20 @@ you."
     (widen)
     (goto-char (point-min))
     (search-forward "\n\n" nil t)
-    (or (and boundary
-	     (re-search-forward boundary nil t)
-	     (forward-line 2))
-	(and (re-search-forward message-unsent-separator nil t)
-	     (forward-line 1))
-	(re-search-forward "^Return-Path:.*\n" nil t))
-    ;; We remove everything before the bounced mail.
-    (delete-region
-     (point-min)
-     (if (re-search-forward "^[^ \n\t]+:" nil t)
-	 (match-beginning 0)
-       (point)))
+    (if (or (and boundary
+		 (re-search-forward boundary nil t)
+		 (forward-line 2))
+	    (and (re-search-forward message-unsent-separator nil t)
+		 (forward-line 1))
+	    (re-search-forward "^Return-Path:.*\n" nil t))
+	;; We remove everything before the bounced mail.
+	(delete-region
+	 (point-min)
+	 (if (re-search-forward "^[^ \n\t]+:" nil t)
+	     (match-beginning 0)
+	   (point)))
+      (when (re-search-backward "^.?From .*\n" nil t)
+	(delete-region (match-beginning 0) (match-end 0))))
     (save-restriction
       (message-narrow-to-head-1)
       (message-remove-header message-ignored-bounced-headers t)
