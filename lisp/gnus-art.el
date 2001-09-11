b92395e6dc07afda2272f69956f802c86dda4135
TSUCHIYA Masatoshi <tsuchiya@namazu.org>
2001-09-11 07:42:35 +0000

    * lisp/gnus-art.el (gnus-request-article-this-buffer): Copy an article from `gnus-original-article-buffer' to `gnus-article-buffer'.

--- lisp/gnus-art.el
+++ lisp/gnus-art.el
@@ -4920,6 +4920,11 @@ T-gnus change: Insert an article into `gnus-original-article-buffer'."
       ;; Associate this article with the current summary buffer.
       (setq gnus-article-current-summary gnus-summary-buffer)
 
+      ;; T-gnus change: Copy the requested article from
+      ;; `gnus-original-article-buffer' to the current buffer when
+      ;; this function is called.
+      (insert-buffer gnus-original-article-buffer)
+
       ;; Decode charsets.
       (run-hooks 'gnus-article-decode-hook)
       ;; Mark article as decoded or not.
