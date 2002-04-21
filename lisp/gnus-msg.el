76ad0ca6bed08c4684af5789d8c850776646441f
Katsumi Yamaoka <yamaoka@jpl.org>
2002-04-21 22:48:47 +0000

    Synch with Oort Gnus.

--- lisp/gnus-msg.el
+++ lisp/gnus-msg.el
@@ -226,6 +226,26 @@ This variable is used only when `gnus-post-method' is `current'."
   :group 'gnus-group-foreign
   :type '(repeat (symbol :tab "Back end")))
 
+(defcustom gnus-message-replysign
+  nil
+  "Automatically sign replys to signed messages.
+See also the `mml-default-sign-method' variable."
+  :group 'gnus-message
+  :type 'boolean)
+
+(defcustom gnus-message-replyencrypt
+  nil
+  "Automatically encrypt replys to encrypted messages.
+See also the `mml-default-encrypt-method' variable."
+  :group 'gnus-message
+  :type 'boolean)
+
+(defcustom gnus-message-replysignencrypted
+  nil
+  "Setting this causes automatically encryped messages to also be signed."
+  :group 'gnus-message
+  :type 'boolean)
+
 ;;; Internal variables.
 
 (defvar gnus-inhibit-posting-styles nil
@@ -1101,7 +1121,21 @@ If VERY-WIDE, make a very wide reply."
 	(goto-char (point-max)))
       (message-reply nil wide)
       (when yank
-	(gnus-inews-yank-articles yank)))))
+	(gnus-inews-yank-articles yank))
+;;      (when (or gnus-message-replysign gnus-message-replyencrypt)
+;;	(let (signed encrypted)
+;;	  (save-excursion
+;;	    (set-buffer (or gnus-article-buffer article-buffer))
+;;	    (setq signed (memq 'signed gnus-article-wash-types))
+;;	    (setq encrypted (memq 'encrypted gnus-article-wash-types)))
+;;	  (cond ((and gnus-message-replysign signed)
+;;		 (mml-secure-message mml-default-sign-method 'sign))
+;;		((and gnus-message-replyencrypt encrypted)
+;;		 (mml-secure-message mml-default-encrypt-method
+;;				     (if gnus-message-replysignencrypted
+;;					 'signencrypt
+;;				       'encrypt))))))
+      )))
 
 (defun gnus-summary-reply-with-original (n &optional wide)
   "Start composing a reply mail to the current message.
