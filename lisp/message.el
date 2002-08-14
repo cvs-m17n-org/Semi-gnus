6c3cfc995ace7a8a875785e68adedc99bfce8b1f
Katsumi Yamaoka <yamaoka@jpl.org>
2002-08-14 02:42:54 +0000

    Synch with Oort Gnus.

--- lisp/message.el
+++ lisp/message.el
@@ -1219,11 +1219,11 @@ candidates:
 	    `((,(concat "^\\(" (regexp-quote mail-header-separator) "\\)$")
 	       1 'message-separator-face))
 	  nil)
-      (,(lambda (limit)
-	  (re-search-forward (concat "^\\("
-				     message-cite-prefix-regexp
-				     "\\).*")
-			     limit t))
+      ((lambda (limit)
+	 (re-search-forward (concat "^\\("
+				    message-cite-prefix-regexp
+				    "\\).*")
+			    limit t))
        (0 'message-cited-text-face))
       (,mime-edit-tag-regexp
        (0 'message-mml-face))))
