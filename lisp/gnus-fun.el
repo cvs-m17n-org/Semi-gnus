ee0abf39d7ac48baaff0b0246b2298e055bf119a
Katsumi Yamaoka <yamaoka@jpl.org>
2003-01-13 22:56:48 +0000

    Synch with Oort Gnus.

--- lisp/gnus-fun.el
+++ lisp/gnus-fun.el
@@ -60,7 +60,7 @@ Output to the current buffer, replace text, and don't mingle error."
 
 ;;;###autoload
 (defun gnus-random-x-face ()
-  "Insert a random X-Face header from `gnus-x-face-directory'."
+  "Return X-Face header data chosen randomly from `gnus-x-face-directory'."
   (interactive)
   (when (file-exists-p gnus-x-face-directory)
     (let* ((files (directory-files gnus-x-face-directory t "\\.pbm$"))
@@ -71,18 +71,31 @@ Output to the current buffer, replace text, and don't mingle error."
 		 (shell-quote-argument file)))))))
 
 ;;;###autoload
+(defun gnus-insert-random-x-face-header ()
+  "Insert a random X-Face header from `gnus-x-face-directory'."
+  (interactive)
+  (let ((data (gnus-random-x-face)))
+    (save-excursion
+      (message-goto-eoh)
+      (if data
+	  (insert "X-Face: " data)
+	(message
+	 "No face returned by `gnus-random-x-face'.  Does %s/*.pbm exist?"
+	 gnus-x-face-directory)))))
+
+;;;###autoload
 (defun gnus-x-face-from-file (file)
-  "Insert an X-Face header based on an image file."
-  (interactive "fImage file name:" )
+  "Insert an X-Face header based on a GIF image file."
+  (interactive "fImage file name: ")
   (when (file-exists-p file)
     (gnus-shell-command-to-string
      (format gnus-convert-image-to-x-face-command
-	     (shell-quote-argument file)))))
+	     (shell-quote-argument (expand-file-name file))))))
 
 ;;;###autoload
 (defun gnus-face-from-file (file)
-  "Return an Face header based on an image file."
-  (interactive "fImage file name:" )
+  "Return an Face header based on a JPEG image file."
+  (interactive "fImage file name: ")
   (when (file-exists-p file)
     (let ((done nil)
 	  (attempt "")
@@ -93,7 +106,7 @@ Output to the current buffer, replace text, and don't mingle error."
 	(setq attempt
 	      (gnus-shell-command-to-string
 	       (format gnus-convert-image-to-face-command
-		       (shell-quote-argument file)
+		       (shell-quote-argument (expand-file-name file))
 		       quant)))
 	(if (> (length attempt) 740)
 	    (progn
