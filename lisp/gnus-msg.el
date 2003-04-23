a5bf2cd5e80bd51f9870fd6f3f6539ce4cd798b3
Katsumi Yamaoka <yamaoka@jpl.org>
2003-04-23 23:09:02 +0000

    Synch to Oort Gnus 200304231953.

--- lisp/gnus-msg.el
+++ lisp/gnus-msg.el
@@ -172,9 +172,6 @@ See Info node `(gnus)Posting Styles'."
   :group 'gnus-message
   :type 'boolean)
 
-(defvar gnus-inews-mark-gcc-as-read nil
-  "Obsolete variable. Use `gnus-gcc-mark-as-read' instead.")
-
 (make-obsolete-variable 'gnus-inews-mark-gcc-as-read
 			'gnus-gcc-mark-as-read)
 
