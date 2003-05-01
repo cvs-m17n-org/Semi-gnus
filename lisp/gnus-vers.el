61bc7ee1145bb9a46e0fea66861140c0f15c273a
Katsumi Yamaoka <yamaoka@jpl.org>
2003-05-01 13:16:09 +0000

    T-gnus 6.15.22 revision 00.

--- lisp/gnus-vers.el
+++ lisp/gnus-vers.el
@@ -40,10 +40,10 @@
 ;; Product information of this gnus.
 (product-provide 'gnus-vers
   (product-define "T-gnus" nil
-		  (list 6 15 20
+		  (list 6 15 22
 			(string-to-number gnus-revision-number))))
 
-(defconst gnus-original-version-number "0.20"
+(defconst gnus-original-version-number "0.22"
   "Version number for this version of Gnus.")
 
 (provide 'running-pterodactyl-gnus-0_73-or-later)
