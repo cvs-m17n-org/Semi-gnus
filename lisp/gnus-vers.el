e19f13ad879b92782e9287791afea2e7e2dfffd9
Katsumi Yamaoka <yamaoka@jpl.org>
2003-05-01 13:25:14 +0000

    T-gnus 6.15.23 (quimby) revision 00.

--- lisp/gnus-vers.el
+++ lisp/gnus-vers.el
@@ -40,10 +40,10 @@
 ;; Product information of this gnus.
 (product-provide 'gnus-vers
   (product-define "T-gnus" nil
-		  (list 6 15 21
+		  (list 6 15 23
 			(string-to-number gnus-revision-number))))
 
-(defconst gnus-original-version-number "0.21"
+(defconst gnus-original-version-number "0.23"
   "Version number for this version of Gnus.")
 
 (provide 'running-pterodactyl-gnus-0_73-or-later)
