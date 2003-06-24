9c962dd0e41092ad23aa3d7b4561fec0915c1cb6
Katsumi Yamaoka <yamaoka@jpl.org>
2003-06-24 22:28:29 +0000

    Synch to Gnus 200306241704.

--- lisp/spam.el
+++ lisp/spam.el
@@ -47,10 +47,6 @@
 ;; for nnimap-split-download-body-default
 (eval-when-compile (require 'nnimap))
 
-;; autoload ietf-drums-parse-addresses
-(eval-and-compile
-  (autoload 'ietf-drums-parse-addresses "ietf-drums"))
-
 ;; autoload query-dig
 (eval-and-compile
   (autoload 'query-dig "dig"))
@@ -1000,7 +996,7 @@ Uses `gnus-newsgroup-name' if category is nil (for ham registration)."
 	  (setq address (buffer-substring (point) (spam-point-at-eol)))
 	  (forward-line 1)
 	  ;; insert the e-mail address if detected, otherwise the raw data
-	  (let ((pure-address (car (ietf-drums-parse-addresses address))))
+	  (let ((pure-address (cadr (gnus-extract-address-components address))))
 	    (push (or pure-address address) contents))))
       (nreverse contents))))
 
@@ -1008,7 +1004,7 @@ Uses `gnus-newsgroup-name' if category is nil (for ham registration)."
   (let ((from (message-fetch-field "from"))
 	found)
     (while cache
-      (let* ((address (pop cache)))
+      (let ((address (pop cache)))
 	(unless (zerop (length address)) ; 0 for a nil address too
 	  (setq address (regexp-quote address))
 	  ;; fix regexp-quote's treatment of user-intended regexes
