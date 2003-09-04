bb05da9dafd798fc947d47183a457352a14c7708
Katsumi Yamaoka <yamaoka@jpl.org>
2003-09-04 22:15:45 +0000

    Synch to Gnus 200309042222.

--- lisp/nnmh.el
+++ lisp/nnmh.el
@@ -325,7 +325,9 @@ as unread by Gnus.")
 	   (not (equal group "draft")))
       (nnmail-check-syntax))
   (when nnmail-cache-accepted-message-ids
-    (nnmail-cache-insert (nnmail-fetch-field "message-id") group))
+    (nnmail-cache-insert (nnmail-fetch-field "message-id") 
+			 group
+			 (nnmail-fetch-field "subject")))
   (nnheader-init-server-buffer)
   (prog1
       (if (stringp group)
