0cf282691990b51508cfcae198b9cdd161a5008f
Katsumi Yamaoka <yamaoka@jpl.org>
2003-12-31 07:39:46 +0000

    Synch to Gnus 200312310312.

--- lisp/message.el
+++ lisp/message.el
@@ -5767,7 +5767,6 @@ If the current line has `message-yank-prefix', insert it on the new line."
     (error
      (split-line))))
      
-
 (defun message-fill-header (header value)
   (let ((begin (point))
 	(fill-column 78)
