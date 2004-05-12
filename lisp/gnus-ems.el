2bcf936c61056b198ab1be91f01cf5763cd79aae
Katsumi Yamaoka <yamaoka@jpl.org>
2004-05-12 21:56:21 +0000

    Synch to No Gnus 200405122100.

--- lisp/gnus-ems.el
+++ lisp/gnus-ems.el
@@ -45,7 +45,6 @@
 (eval-and-compile
   (autoload 'gnus-xmas-define "gnus-xmas")
   (autoload 'gnus-xmas-redefine "gnus-xmas")
-  (autoload 'appt-select-lowest-window "appt")
   (autoload 'gnus-get-buffer-create "gnus")
   (autoload 'nnheader-find-etc-directory "nnheader"))
 
@@ -147,6 +146,18 @@
 	    `(let ((val (eval ,el)))
 	       (gnus-truncate-string val (string-width val) ,cut))))))
     ))
+
+;; Clone of `appt-select-lowest-window' in appt.el.
+(defun gnus-select-lowest-window ()
+"Select the lowest window on the frame."
+  (let ((lowest-window (selected-window))
+	(bottom-edge (nth 3 (window-edges))))
+    (walk-windows (lambda (w)
+		    (let ((next-bottom-edge (nth 3 (window-edges w))))
+		      (when (< bottom-edge next-bottom-edge)
+			(setq bottom-edge next-bottom-edge
+			      lowest-window w)))))
+    (select-window lowest-window)))
 
 (defun gnus-region-active-p ()
   "Say whether the region is active."
