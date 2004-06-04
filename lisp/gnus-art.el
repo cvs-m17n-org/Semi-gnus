c2020cc2c69f9a82072e8defe2dfdfd00b2d65d9
Katsumi Yamaoka <yamaoka@jpl.org>
2004-06-04 07:03:58 +0000

    Synch to No Gnus 200406040702.

--- lisp/gnus-art.el
+++ lisp/gnus-art.el
@@ -1697,25 +1697,34 @@ Initialized from `text-mode-syntax-table.")
 	(when (eq 1 (point-min))
 	  (set-window-start (get-buffer-window (current-buffer)) 1)))
     (unless gnus-inhibit-hiding
-      (save-excursion
-	(save-restriction
-	  (let ((inhibit-read-only t)
-		(case-fold-search t)
-		(max (1+ (length gnus-sorted-header-list)))
-		(ignored (when (not gnus-visible-headers)
-			   (cond ((stringp gnus-ignored-headers)
-				  gnus-ignored-headers)
-				 ((listp gnus-ignored-headers)
-				  (mapconcat 'identity gnus-ignored-headers
-					     "\\|")))))
-		(visible
-		 (cond ((stringp gnus-visible-headers)
-			gnus-visible-headers)
-		       ((and gnus-visible-headers
-			     (listp gnus-visible-headers))
-			(mapconcat 'identity gnus-visible-headers "\\|"))))
-		(inhibit-point-motion-hooks t)
-		beg)
+      (let ((inhibit-read-only t)
+	    (case-fold-search t)
+	    (max (1+ (length gnus-sorted-header-list)))
+	    (inhibit-point-motion-hooks t)
+	    ignored visible beg)
+	(save-excursion
+	  ;; `gnus-ignored-headers' and `gnus-visible-headers' may be
+	  ;; group parameters, so we should go to the summary buffer.
+	  (when (prog1
+		    (condition-case nil
+			(progn (set-buffer gnus-summary-buffer) t)
+		      (error nil))
+		  (setq ignored (when (not gnus-visible-headers)
+				  (cond ((stringp gnus-ignored-headers)
+					 gnus-ignored-headers)
+					((listp gnus-ignored-headers)
+					 (mapconcat 'identity
+						    gnus-ignored-headers
+						    "\\|"))))
+			visible (cond ((stringp gnus-visible-headers)
+				       gnus-visible-headers)
+				      ((and gnus-visible-headers
+					    (listp gnus-visible-headers))
+				       (mapconcat 'identity
+						  gnus-visible-headers
+						  "\\|")))))
+	    (set-buffer gnus-article-buffer))
+	  (save-restriction
 	    ;; First we narrow to just the headers.
 	    (article-narrow-to-head)
 	    ;; Hide any "From " lines at the beginning of (mail) articles.
