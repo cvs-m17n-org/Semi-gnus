9e12a17700d75935492cea5769423eb07ea2e4c8
Katsumi Yamaoka <yamaoka@jpl.org>
2004-09-20 22:10:39 +0000

    Synch to No Gnus 200409201204.

--- lisp/gnus-sum.el
+++ lisp/gnus-sum.el
@@ -1181,6 +1181,7 @@ type of files to save."
 This is mostly relevant for slow back ends where the user may
 wish to widen the summary buffer to include all headers
 that were fetched.  Say, for nnultimate groups."
+  :version "21.4"
   :group 'gnus-summary
   :type '(choice boolean regexp))
 
