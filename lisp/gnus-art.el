7e54e538b25e898415c45e0f92a40d289f237696
Katsumi Yamaoka <yamaoka@jpl.org>
2004-11-15 03:55:25 +0000

    Synch to No Gnus 200411142346.

--- lisp/gnus-art.el
+++ lisp/gnus-art.el
@@ -6794,6 +6794,8 @@ variable it the real callback function."
      0 (>= gnus-button-browse-level 0) browse-url 0)
     ("^[^:]+:" gnus-button-url-regexp
      0 (>= gnus-button-browse-level 0) browse-url 0)
+    ("^OpenPGP:.*url=" gnus-button-url-regexp
+     0 (>= gnus-button-browse-level 0) gnus-button-openpgp 0)
     ("^[^:]+:" "\\bmailto:\\([-a-z.@_+0-9%=?&/]+\\)"
      0 (>= gnus-button-message-level 0) gnus-url-mailto 1)
     ("^[^:]+:" "\\(<\\(url: \\)?\\(nntp\\|news\\):\\([^>\n ]*\\)>\\)"
@@ -7198,6 +7200,13 @@ specified by `gnus-button-alist'."
   (info)
   (Info-directory)
   (Info-menu url))
+
+(defun gnus-button-openpgp (url)
+  "Retrieve and add an OpenPGP key given URL from an OpenPGP header."
+  (with-temp-buffer
+    (mm-url-insert-file-contents-external url)
+    (pgg-snarf-keys-region (point-min) (point-max))
+    (pgg-display-output-buffer nil nil nil)))
 
 (defun gnus-button-message-id (message-id)
   "Fetch MESSAGE-ID."
