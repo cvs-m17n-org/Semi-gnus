fa822fb54b0fd0929bf7363ead510ed52e1a97a5
Katsumi Yamaoka <yamaoka@jpl.org>
2005-05-01 23:51:08 +0000

    Synch to No Gnus 200505011705.

--- lisp/gnus-vers.el
+++ lisp/gnus-vers.el
@@ -40,10 +40,10 @@
 ;; Product information of this gnus.
 (product-provide 'gnus-vers
   (product-define "T-gnus" nil
-		  (list 6 17 3
+		  (list 6 17 4
 			(string-to-number gnus-revision-number))))
 
-(defconst gnus-original-version-number "0.3"
+(defconst gnus-original-version-number "0.4"
   "Version number for this version of Gnus.")
 
 (provide 'running-pterodactyl-gnus-0_73-or-later)
