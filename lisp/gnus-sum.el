dbb49776e7dc768ab94c07fa68a44cef6d8ccbbe
Katsumi Yamaoka <yamaoka@jpl.org>
2005-09-25 22:17:16 +0000

    Synch to No Gnus 200509252127.

--- lisp/gnus-sum.el
+++ lisp/gnus-sum.el
@@ -7816,8 +7816,8 @@ articles that are younger than AGE days."
 	   (gnus-completing-read-with-default
 	    (symbol-name (car gnus-extra-headers))
 	    (if current-prefix-arg
-		"Exclude extra header:"
-	      "Limit extra header:")
+		"Exclude extra header"
+	      "Limit extra header")
 	    (mapcar (lambda (x)
 		      (cons (symbol-name x) x))
 		    gnus-extra-headers)
@@ -9495,7 +9495,7 @@ latter case, they will be copied into the relevant groups."
 					gnus-newsgroup-name)))))
 		(method
 		 (gnus-completing-read-with-default
-		  methname "What backend do you want to use when respooling?"
+		  methname "Backend to use when respooling"
 		  methods nil t nil 'gnus-mail-method-history))
 		ms)
 	   (cond
@@ -11281,7 +11281,7 @@ save those articles instead."
   (let* ((split-name (gnus-get-split-value gnus-move-split-methods))
 	 (minibuffer-confirm-incomplete nil) ; XEmacs
 	 (prom
-	  (format "%s %s to:"
+	  (format "%s %s to"
 		  prompt
 		  (if (> (length articles) 1)
 		      (format "these %d articles" (length articles))
