df5dd55048e268f654268e437c6cf686b525a113
Katsumi Yamaoka <yamaoka@jpl.org>
2005-09-28 06:10:01 +0000

    Synch to No Gnus 200509280609.

--- lisp/nntp.el
+++ lisp/nntp.el
@@ -222,9 +222,6 @@ then use this hook to rsh to the remote machine and start a proxy NNTP
 server there that you can connect to.  See also
 `nntp-open-connection-function'")
 
-(defvoo nntp-warn-about-losing-connection t
-  "*If non-nil, beep when a server closes connection.")
-
 ;; Marks
 (defvoo nntp-marks-is-evil nil
   "*If non-nil, Gnus will never generate and use marks file for nntp groups.
