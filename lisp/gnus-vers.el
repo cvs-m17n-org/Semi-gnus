f293956551436bbc3269537d943adca8884250f9
Katsumi Yamaoka <yamaoka@jpl.org>
2006-04-11 22:58:21 +0000

    T-gnus 6.17.4.

--- lisp/gnus-vers.el
+++ lisp/gnus-vers.el
@@ -1,6 +1,6 @@
 ;;; gnus-vers.el --- Declare gnus version
 
-;; Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005
+;; Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2006
 ;; Free Software Foundation, Inc.
 
 ;; Author: Keiichi Suzuki <keiichi@nanap.org>
